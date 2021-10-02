1fde0c475c53dfc0e7a439863b7a7d2f28a7ab3d
rajrachchh14 <rajrachchh14@gmail.com>
2021-10-02 15:48:40 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
+import ImageCard from './ImageCard';
 
 export default function App() {
   return (
     <>
       <div className="container">
         <div className="row">
-          <div className="col-12 col-lg-3">
-            <div className="card" style={{ width: '18rem' }}>
-              <img
-                src="https://www.w3schools.com/css/img_chania.jpg"
-                className="card-img-top"
-                alt="..."
-              />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <span className="badge bg-secondary">New</span>
-              </div>
-            </div>
-          </div>
+          <ImageCard />
         </div>
       </div>
     </>
