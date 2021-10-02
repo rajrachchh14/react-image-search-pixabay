dbfe475204ab36fafc8600a57c34fb3d4ee261dd
rajrachchh14 <rajrachchh14@gmail.com>
2021-10-02 16:23:19 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ImageCard from './ImageCard';
 // REACT_API_PIXAL_KEY
 export default function App() {
   const [img, setImage] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
 
   return (
     <>
