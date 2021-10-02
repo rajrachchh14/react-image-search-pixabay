e06c0cd9ef312e18c3754793627707ffa509470d
rajrachchh14 <rajrachchh14@gmail.com>
2021-10-02 16:20:30 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ImageCard from './ImageCard';
 // REACT_API_PIXAL_KEY
 export default function App() {
+  const [img, setImage] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
   return (
     <>
       <div className="container">
