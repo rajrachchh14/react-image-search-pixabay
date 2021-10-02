fa7166f5b7e7afec6c14ab3859d3b6432abfbe24
rajrachchh14 <rajrachchh14@gmail.com>
2021-10-02 20:09:07 +0530

    1 file modified

--- src/ImageSearch.js
+++ src/ImageSearch.js
@@ -4,17 +4,19 @@ export default function ImageSearch() {
   const [text, setText] = useState();
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="row g-3 align-items-center">
-            <div className="col-auto">
-              <label for="inputPassword6" className="col-form-label">
-                <input type="password" className="form-control" />
-              </label>
-            </div>
-            <div className="col-auto">
-              <button className="btn btn-primary">Search</button>
-            </div>
+      <div className="container d-flex justify-content-center p-3">
+        <div className="row align-items-center">
+          <div className="col-auto">
+            <label className="col-form-label">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search Image"
+              />
+            </label>
+          </div>
+          <div className="col-auto">
+            <button className="btn btn-primary">Search</button>
           </div>
         </div>
       </div>
