953a6fa5b2f581501febe54edc134c8133004ec2
rajrachchh14 <rajrachchh14@gmail.com>
2021-10-02 16:51:15 +0530

    1 file modified, 1 deleted

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ export default function App() {
   const [term, setTerm] = useState('');
 
   useEffect(() => {
-    const url = `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXAL_KEY}&q=yellow+flowers&image_type=photo&pretty=true`;
-    console.log(process.env.REACT_APP_PIXAL_KEY);
+    const key = '23664585-b63ea49e0412f4d30e9b28cc8';
+    const url = `https://pixabay.com/api/?key=${key}&q=yellow+flowers&image_type=photo&pretty=true`;
+    console.log(url);
     fetch(url)
       .then((res) => res.json())
       .then(
